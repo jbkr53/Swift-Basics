1e0e504bc4418c73a0d7a3f0e6390a4d5080c318
James Kirkland <jbkr53@mail.umkc.edu>
2019-09-05 10:17:47 -0500

    created initial Xcode project

---
+++ Swift-Basics/Swift-Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Basics
+//
+//  Created by James Kirkland on 9/5/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
